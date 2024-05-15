24f4d9f5c1546ccad1f0654bc69e5e3adde69571
hoiboi <gentledemeiz@gmail.com>
2024-05-15 22:26:39 +0300

    start project

---
+++ src/main/java/ru/theft/obratka/ObratkaApplication.java
@@ -0,0 +1,13 @@
+package ru.theft.obratka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObratkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObratkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/theft/obratka/ObratkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.theft.obratka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObratkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
